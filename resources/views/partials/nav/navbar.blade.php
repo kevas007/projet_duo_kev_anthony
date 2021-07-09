7521831579c3a346a62f31421d757812bd5d6f51
kevas007 <kevassiobo@gmail.com>
2021-07-09 16:10:40 +0200

    base2

---
+++ resources/views/partials/nav/navbar.blade.php
@@ -0,0 +1,33 @@
+<header id="masthead" class="site-header" data-anchor-target=".hero" data-top="background: rgba(59,58,54,0); border-bottom-color: rgba(226,226,226,0);" data-top-bottom="background: rgba(59,58,54,1); border-bottom-color: rgba(226,226,226,1);">
+
+    <nav id="primary-navigation" class="site-navigation">
+
+        <div class="container-fluid">
+
+            <div class="navbar-header">
+
+                <a href="index.html" class="site-title yellow-text">Business Perfect</a>
+
+            </div><!-- /.navbar-header -->
+
+            <div class="main-menu" id="perfect-navbar-collapse">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="active">
+                        <a href="index.html">Home</a></li>
+                    <li>
+                        <a href="blog.html">Blog </a>
+                    </li>
+                    <li>
+                        <a href="portfolio.html">Portfolio</a>
+                    </li>
+                    <li>
+                        <a href="contact.html">Contact</a>
+                    </li>
+                </ul><!-- /.navbar-nav -->
+            </div><!-- /.navbar-collapse -->
+
+        </div>
+
+    </nav><!-- /.site-navigation -->
+
+</header><!-- /#masthead -->
