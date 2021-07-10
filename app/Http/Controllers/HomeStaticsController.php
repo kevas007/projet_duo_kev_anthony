5ff5d45eb06ab527110e4516e98aba12372b3fad
kevas007 <kevassiobo@gmail.com>
2021-07-10 12:38:40 +0200

    ajout des migrations

--- app/Http/Controllers/HomeStaticsController.php
+++ app/Http/Controllers/HomeStaticsController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\HomeStatics;
 use Illuminate\Http\Request;
 
 class HomeStaticsController extends Controller
 {
-    //
+    public function store(Request $request){
+        $store = new HomeStatics;
+        $store->titreHome = $request->titreHome;
+        $store->para1 = $request->para1;
+        $store->para2 = $request->para2;
+        $store->sous_titre1 = $request->sous_titre1;
+        $store->para3 = $request->para3;
+        $store->ahref = $request->ahref;
+        $store->sous_titre2 = $request->sous_titre2;
+        $store->para4 = $request->para4;
+        $store->titreBlog = $request->titreBlog;
+        $store->sous_titreBlog = $request->sous_titreBlog;
+        $store->paraBlog = $request->paraBlog;
+        $store->titrePortfolio = $request->titrePortfolio;
+        $store->sous_titrePortfolio = $request->sous_titrePortfolio;
+        $store->btn_sousTitre = $request->btn_sousTitre;
+        $store->titreContact = $request->titreContact;
+        $store->sous_titreContact = $request->sous_titreContact;
+        $store->paraContact = $request->paraContact;
+        $store->companyName = $request->companyName;
+        $store->phone = $request->phone;
+        $store->fax = $request->fax;
+        $store->email = $request->email;
+        $store->save();
+        return redirect('/');
+    }
 }
---
+++ database/migrations/2021_07_10_091621_create_home_dynamiques_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateHomeDynamiquesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('home_dynamiques', function (Blueprint $table) {
+            $table->id();
+            $table->string('h4_1_service');
+            $table->string('p1_service');
+            $table->string('h4_2_service');
+            $table->string('p2_service');
+            $table->string('h4_3_service');
+            $table->string('p3_service');
+            $table->string('h4_4_service');
+            $table->string('p4_service');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('home_dynamiques');
+    }
+}
