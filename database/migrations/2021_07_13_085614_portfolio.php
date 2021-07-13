1310e86f3ff214ec4f2fed0c6ce53947b1098209
blakfreeman <61966557+blakfreeman@users.noreply.github.com>
2021-07-13 11:49:22 +0200

    avanger

--- database/migrations/2021_07_13_085614_portfolio.php
+++ database/migrations/2021_07_13_085614_portfolio.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class Portfolios extends Migration
+class Portfolio extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,11 @@ class Portfolios extends Migration
      */
     public function up()
     {
-
-        Schema::create('portfolios', function (Blueprint $table) {
+         Schema::create('portfolio', function (Blueprint $table) {
             $table->id();
-            $table->string('titre_image');
-            $table->string('para');
+            $table->string('titre');
+            $table->string('paragraphe');
+            $table->string('img');
             $table->timestamps();
         });
     }
--- resources/views/partials/portfolio/portfolio.blade.php
+++ resources/views/partials/portfolio/portfolio.blade.php
@@ -10,202 +10,21 @@
 
         <div class="container-fluid">
             <div class="row" id="grid">
+                @foreach ($portfolios as $portfolio )
                 <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["uiux"]'>
                     <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-1.jpg" alt="" class="img-carousel">
+                        <img src="{{asset('assets/img/'.$portfolio->img)}}" alt="" class="img-carousel">
                         <div class="portfolio-info">
                             <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
+                                <h3>{{$portfolio->titre}}</h3>
                             </div><!-- /.portfolio-info-top -->
                             <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
+                                <p>{{$portfolio->paragraphe}}</p>
                             </div><!-- /.portfolio-info-bottom -->
                         </div><!-- /.portfolio-info  -->
                     </a>
                 </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["branding"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-2.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["identity"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-3.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["uiux"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-4.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["illustrations"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-5.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["identity"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-6.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["uiux"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-7.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["branding"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-8.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["uiux"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-9.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["branding"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-10.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["illustrations"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-2.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["illustrations"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-9.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["identity"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-5.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["branding"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-6.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-                <div class="col-lg-fifth col-md-3 col-sm-4 col-xs-6" data-groups='["illustrations"]'>
-                    <a class="portfolio-link" href="#">
-                        <img src="assets/img/portfolio-3.jpg" alt="" class="img-carousel">
-                        <div class="portfolio-info">
-                            <div class="portfolio-info-top">
-                                <h3>Business Perfect item</h3>
-                            </div><!-- /.portfolio-info-top -->
-                            <div class="portfolio-info-bottom">
-                                <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras viverra dolor eu nunc porttitor sollicitudin. Maecenas dignissim ultricies pharetra.</p>
-                            </div><!-- /.portfolio-info-bottom -->
-                        </div><!-- /.portfolio-info  -->
-                    </a>
-                </div><!-- /.col-lg-fifth  -->
-
+                @endforeach
             </div><!-- /#grid -->
 
         </div>
